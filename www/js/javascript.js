3c88ea568c38be08e80d8d9d21185e2ab5866398
JordiJacas <jjacasventura@iesesteveterradas.cat>
2019-03-11 20:18:05 +0100

    localstorage

--- www/js/javascript.js
+++ www/js/javascript.js
@@ -1,16 +1,44 @@
+var count = 0;
 
 function addItem() {
   var text = $("#tf").val();
 
-  var list = $("#list");
+  /*var list = $("#list");
 
   var button = ("<button type='button' onclick='deleteItem(this.parentNode)'>Delete</button>");
 
   var node = ("<li> "+ text + button + "</li>");
 
-  list.append(node);
+  list.append(node);*/
+
+  localStorage.setItem(count, text);
+  count++;
+
+  updateList();
 }
 
 function deleteItem(item){
-  item.remove();
+  //item.remove();
+
+  localStorage.removeItem(localStorage.key(item));
+
+  updateList();
 }
+
+function updateList(){
+  $('li').remove();
+  for (var i = 0; i < localStorage.length; i++){
+    count++;
+    var list = $("#list");
+
+    var button = ("<button type='button' onclick='deleteItem("+i+")'>Delete</button>");
+
+    var node = ("<li> "+ localStorage.getItem(localStorage.key(i)) + button + "</li>");
+
+    list.append(node);
+
+    console.log(localStorage.getItem(localStorage.key(i)));
+  }
+}
+
+$(document).ready(function(){updateList()});
